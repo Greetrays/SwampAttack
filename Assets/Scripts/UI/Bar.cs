387ce19b9c51cecde2dba73a93b710fca1a5c4b4
Ренат <syltan.100renrgen@gmail.com>
2022-01-08 18:58:19 +0300

    add bars

---
+++ Assets/Scripts/UI/Bar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))] 
+
+public abstract class Bar : MonoBehaviour
+{
+    protected Slider Slider;
+
+    private void Awake()
+    {
+        Slider = GetComponent<Slider>();
+    }
+
+    protected void OnChangedValue(int value, int maxValue)
+    {
+        Slider.value = (float)value / maxValue;
+    }
+}
---
+++ Assets/Scripts/UI/WaveBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class WaveBar : Bar
+{
+    [SerializeField] private SpawnerEnemy _spawner;
+
+    private void OnEnable()
+    {
+        _spawner.ChangedWave += OnChangedValue;
+        Slider.value = 0;
+    }
+
+    private void OnDisable()
+    {
+        _spawner.ChangedWave -= OnChangedValue;
+    }
+}
